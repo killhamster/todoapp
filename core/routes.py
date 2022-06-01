80df0979b6236ddd8dc4a62cf4f4b372bc8c6506
KH <killhamster@gmail.com>
2022-06-01 13:43:58 -0700

    adding error handling for blank items

--- core/routes.py
+++ core/routes.py
@@ -1,16 +1,20 @@
 from core import app, db
 from core.models import Todo
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, Markup, render_template, request, flash, redirect, url_for
+
+blank_error = Markup('<div class="header">ToDo item cannot be blank.</div><p>You need to enter an item.</p>')
 
 @app.route("/")
 def home():
     todo_list = Todo.query.all()
     return render_template("index.html", todo_list=todo_list)
 
-
 @app.route("/add", methods=["POST"])
 def add():
     title = request.form.get("title")
+    if not title:
+        flash(blank_error)
+        return redirect(url_for("home"))
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
     db.session.commit()
